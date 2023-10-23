193f4c3d6310d775327d1b0aeabd678baad57a29
maieka <141380938+maieka@users.noreply.github.com>
2023-10-23 10:43:55 +0700

    Add files via upload

---
+++ tugasphp/tugas2.php
@@ -0,0 +1,10 @@
+<?php
+  $star=9;
+  
+  for($a=1; $a<=$star; $a++){
+        for($c=$star; $c>=$a; $c-=1){
+               echo "*";
+        }
+         echo "<br>";
+   }
+?>
---
+++ tugasphp/tugas3.php
@@ -0,0 +1,10 @@
+<?php
+// rata atas kiri
+
+for($i=0; $i<=8; $i++){
+for($x=0; $x<=$i; $x++){
+echo $x;
+}
+echo "<br />";
+}
+?>
